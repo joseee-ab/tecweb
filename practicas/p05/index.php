c1128d9ae00f98032d8b3216c4d4f671d12a48e7
JoseAB <joseambrosio1705@gmail.com>
2025-10-01 01:25:18 -0600

    Se realizó el punto 4 de la practica 08

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8" />
     <title>Práctica 3</title>
 </head>
 <body>
@@ -32,59 +32,78 @@
 
         echo '<h2>Ejercicio 2</h2>';
         echo '<p>Proporcionar los valores de $a, $b, $c como sigue:</p>'; 
-            echo '<p>Inciso a:</p>'; 
+            echo '<p>Inciso a:</p>';
             $a = "ManejadorSQL";
             $b = 'MySQL';
             $c = &$a;
             echo '<ul>';
-                echo '<li>$a = '."$a";
-                echo '<li>$b = '."$b";
-                echo '<li>$c = '."$c";   
+                echo '<li>$a = '."$a".'</li>';
+                echo '<li>$b = '."$b".'</li>';
+                echo '<li>$c = '."$c".'</li>';   
             echo '</ul>';
             echo '<p>Inciso b:</p>'; 
             $a = "PHP server";
             $b = &$a;
             echo '<ul>';
-                echo '<li>$a = '."$a";
-                echo '<li>$b = '."$b";
-                echo '<li>$c = '."$c";   
+                echo '<li>$a = '."$a".'</li>';
+                echo '<li>$b = '."$b".'</li>';
+                echo '<li>$c = '."$c".'</li>';   
             echo '</ul>';
 
         echo '<h2>Ejercicio 3</h2>';
         echo '<p>Muestra el contenido de cada variable inmediatamente después de cada asignación,
-        verificar la evolución del tipo de estas variables (imprime todos los componentes de los
-        arreglo):</p>';
-            $a = "PHP5";
-            echo "\$a = $a <br/>";
-            $z[] = &$a;
-            echo '$z = ';
-            var_dump($z);
-            echo "<br/>";
-            $b = "5a version de PHP";
-            echo "\$b = $b <br/>";
-            $c = (int)$b*10;
-            echo "\$c = $c <br/>";
-            $a .= $b;
-            echo "\$a = $a <br/>";
-            $b *= $c;
-            echo "\$b = $b <br/>";
-            $z[0] = "MySQL";
-            echo '$z = ';
-            var_dump($z);
+        verificar la evolución del tipo de estas variables (imprime todos los componentes del arreglo):</p>';
+
+        $a = "PHP5";
+        echo "<p>\$a = $a</p>";
+
+        $z[] = &$a;
+        echo '<p>$z = </p><pre>';
+        print_r($z);
+        echo '</pre>';
+
+        $b = "5a version de PHP";
+        echo "<p>\$b = $b</p>";
+
+        $c = (int)$b*10;
+        echo "<p>\$c = $c</p>";
+
+        $a .= $b;
+        echo "<p>\$a = $a</p>";
+
+        $b *= $c;
+        echo "<p>\$b = $b</p>";
+
+        $z[0] = "MySQL";
+        echo '<p>$z = </p><pre>';
+        print_r($z);
+        echo '</pre>';
 
         echo '<h2>Ejercicio 4</h2>';
-        echo '<p>Lee y muestra los valores de las variables del ejercicio anterior, pero ahora con la ayuda de
-        la matriz $GLOBALS o del modificador global de PHP.</p>';
-            echo "a: ".$GLOBALS['a']."<br>";
-            echo "z: "; 
-            print_r($GLOBALS['z']);
-            echo "<br>";
-            echo "b: ".$GLOBALS['b']."<br/>";
-            echo "c: ".$GLOBALS['c']."<br/>";
-            echo "a: ".$GLOBALS['a']."<br/>";
-            echo "b: ".$GLOBALS['b']."<br/>";
-            echo "z: "; 
-            print_r($GLOBALS['z']);
+        echo '<p>Lee y muestra los valores de las variables del ejercicio anterior usando $GLOBALS:</p>';
+
+        echo "<p>a: ".$GLOBALS['a']."</p>";
+        echo "<p>z: </p><pre>";
+        print_r($GLOBALS['z']);
+        echo "</pre>";
+        echo "<p>b: ".$GLOBALS['b']."</p>";
+        echo "<p>c: ".$GLOBALS['c']."</p>";
+
+        echo '<h2>Ejercicio 4</h2>';
+        echo '<p>Lee y muestra los valores de las variables del ejercicio anterior usando $GLOBALS:</p>';
+        echo '<p>a: '.$GLOBALS['a'].'</p>';
+        echo '<p>z:</p>';
+        echo '<pre>';
+        print_r($GLOBALS['z']);
+        echo '</pre>';
+        echo '<p>b: '.$GLOBALS['b'].'</p>';
+        echo '<p>c: '.$GLOBALS['c'].'</p>';
+        echo '<p>a (otra vez): '.$GLOBALS['a'].'</p>';
+        echo '<p>b (otra vez): '.$GLOBALS['b'].'</p>';
+        echo '<p>z (otra vez):</p>';
+        echo '<pre>';
+        print_r($GLOBALS['z']);
+        echo '</pre>';
         
         echo '<h2>Ejercicio 5</h2>';
         echo '<p>Dar el valor de las variables $a, $b, $c al final del siguiente script:</p>'; 
@@ -92,16 +111,16 @@
         $a = "7 personas";
         $b = (integer) $a;
         $a = "9E3";
-        $c = (double) $a; 
+        $c = (double) $a;
 
-        echo "a: $a<br/>";
-        echo "b: $b<br/>";
-        echo "c: $c<br/>";
-        
+        echo '<p>a: '.$a.'</p>';
+        echo '<p>b: '.$b.'</p>';
+        echo '<p>c: '.$c.'</p>';
+
         echo '<h2>Ejercicio 6</h2>';
-        echo '<p>Dar y comprobar el valor booleano de las variables $a, $b, $c, $d, $e y $f y muéstralas
-        usando la función var_dump(<datos>).</p>';
-        unset($a, $b, $c);  
+        echo '<p>Dar y comprobar el valor booleano de las variables $a, $b, $c, $d, $e y $f usando var_dump:</p>';
+
+        unset($a, $b, $c);
 
         $a = "0";
         $b = "TRUE";
@@ -109,36 +128,42 @@
         $d = ($a OR $b);
         $e = ($a AND $c);
         $f = ($a XOR $b);
-        echo '$a = ';
-        var_dump($a);
-        echo "<br/>";
-        echo '$b = ';
-        var_dump($b);
-        echo "<br/>";
-        echo '$c = ';
-        var_dump($c);
-        echo "<br/>";
-        echo '$d = ';
-        var_dump($d);
-        echo "<br/>";
-        echo '$e = ';
-        var_dump($e);
-        echo "<br/>";
-        echo '$f = ';
-        var_dump($f);
-        echo "<br/>";
 
-        echo '<p>Después investiga una función de PHP que permita transformar el valor booleano de $c y $e
-        en uno que se pueda mostrar con un echo:</p>';
-        echo "c = " . var_export($c, true) . "<br/>";
-        echo "e = " . var_export($e, true) . "<br/>";
+        echo '<p>$a:</p>';
+        echo '<pre>'; var_dump($a); echo '</pre>';
+
+        echo '<p>$b:</p>';
+        echo '<pre>'; var_dump($b); echo '</pre>';
+
+        echo '<p>$c:</p>';
+        echo '<pre>'; var_dump($c); echo '</pre>';
+
+        echo '<p>$d:</p>';
+        echo '<pre>'; var_dump($d); echo '</pre>';
+
+        echo '<p>$e:</p>';
+        echo '<pre>'; var_dump($e); echo '</pre>';
+
+        echo '<p>$f:</p>';
+        echo '<pre>'; var_dump($f); echo '</pre>';
+
+        echo '<p>Transformando booleanos para mostrar con echo:</p>';
+        echo '<p>c = '.var_export($c, true).'</p>';
+        echo '<p>e = '.var_export($e, true).'</p>';
+
+        echo '<p>Después investiga una función de PHP que permita transformar el valor booleano de $c y $e en uno que se pueda mostrar con un echo:</p>';
+        echo '<p>c = '.var_export($c, true).'</p>';
+        echo '<p>e = '.var_export($e, true).'</p>';
 
         echo '<h2>Ejercicio 7</h2>';
         echo '<p>Usando la variable predefinida $_SERVER, determina lo siguiente:</p>';
         echo '<ul>';
-        echo '<li> a. La versión de Apache y PHP'.phpversion().$_SERVER['SERVER_SOFTWARE']."</li>";
-        echo '<li> b. El nombre del sistema operativo (servidor)'.PHP_OS."</li>";
-        echo '<li> c. El idioma del navegador (cliente)'. $_SERVER['HTTP_ACCEPT_LANGUAGE']."</li>";
+        echo '<li>a. La versión de Apache y PHP: '.phpversion().' '.$_SERVER['SERVER_SOFTWARE'].'</li>';
+        echo '<li>b. El nombre del sistema operativo (servidor): '.PHP_OS.'</li>';
+        echo '<li>c. El idioma del navegador (cliente): '.$_SERVER['HTTP_ACCEPT_LANGUAGE'].'</li>';
+        echo '</ul>';
+
+        echo '<p> <a href="https://validator.w3.org/check?uri=referer"><img src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a> echo</p>';
     ?>
 </body>
 </html>
